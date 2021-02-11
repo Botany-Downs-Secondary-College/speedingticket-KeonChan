e26aea44c41f048407d518b72f4c7737ca8b6113
KeonChan <78829739+KeonChan@users.noreply.github.com>
2021-02-11 15:05:27 +1300

    Update speed_ticket_v2.py

--- speed_ticket_v2.py
+++ speed_ticket_v2.py
@@ -1,8 +1,8 @@
 #variable list
-wantedList =['tomson','kelly','andrew','kei']
+wantedList =['tomson','kelly','andrew','kei'] #wanted list
 
 #function wiht parameter
-def calculateFine(speed_v, speed_l):
+def calculateFine(speed_v, speed_l): #calculate the fine depending on how much the driver surpass the speed limit
     
     over = speed_v - speed_l
     fine = over * 10
@@ -20,15 +20,15 @@ def calculateFine(speed_v, speed_l):
 name = input("What is your name?")
 if name in wantedList:
     print(name.upper(),",You are now under arrest!")
-else:
+else: #ask information needed for the fine calculation
     repeat = True
     while repeat == True:
         try:
             speed_v = float(input("What's the speed of your vehical?"))
             speed_l = float(input("What's the speed limit there?"))
-            repeat = False
+            repeat = False #stop the loop
         except ValueError:
-            print("PLease enter in digits")
+            print("PLease enter in digits") #demends the user to enter in digit
     calculateFine(speed_v, speed_l)
     
 
